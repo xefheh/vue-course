ba8a3c1c42b305f0a400426bfab2f86ff9bae796
xefh <xefheh@gmail.com>
2025-07-17 03:22:04 +0500

    2-events
    - Добавил компоненты карты
    - Добавил компонент счёта
    - Вывенл SVG в отдельный компонент
    - Добавил компоненту карты emit для будущей логики (переворот, изменение статуса)

---
+++ src/components/Score.vue
@@ -0,0 +1,29 @@
+<script setup>
+import ScoreIcon from './ScoreIcon.vue';
+
+const { score = 100} = defineProps({
+	score: Number
+ })
+</script>
+
+<template>
+<div class="score">
+	{{ score }}
+	<ScoreIcon/>
+</div>
+</template>
+
+<style scoped>
+	.score {
+		color: var(--primary);
+		font-weight: 700;
+		padding: 12px 24px;
+		background-color: var(--color-primary-light);
+		border-radius: 36px;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 10px;
+	}
+</style>
